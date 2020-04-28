59485f7392cec25927b92addefd645ee0d9a6476
charlescqian <charlesqian.ubc@gmail.com>
2020-04-28 11:48:52 -0700

    Updates

---
+++ precache-manifest.a26df5bf97a0691fabe277334abaa7e5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abdce75a2f3a2c8d3df5426f0494320a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e3a3dcb6ea2122c51dcb",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "dee8dad4d52371098f18",
+    "url": "/static/js/2.dcc97af0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.dcc97af0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3a3dcb6ea2122c51dcb",
+    "url": "/static/js/main.5e151638.chunk.js"
+  },
+  {
+    "revision": "97fdbc0ec084af591aa3",
+    "url": "/static/js/runtime-main.d03c7fcd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
