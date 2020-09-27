2ee00e25e0a7b027fbc28313ba8c69e476171987
charlescqian <charlesqian.ubc@gmail.com>
2020-09-26 19:14:09 -0700

    Added smooth scrolling to sidebar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Header from './components/Header';
 import Footer from './components/Footer';
 import Section from './components/Section';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Sidebar from './components/Sidebar';
+import dummyText from "./DummyText";
 
 class App extends React.Component {
   render() {
@@ -13,14 +13,36 @@ class App extends React.Component {
       <div className="App">
         {/* <Header /> */}
         <Sidebar />
-        <Section id="introduction"/>
-        <Section id="about"/>
-        <Section id="projects"/>
-        <Section id="extras"/>
-        <Section />
-        <Section />
-        <Section />
-        <Section />
+        <Section 
+          id="home" 
+          subtitle={dummyText}
+          dark={true}
+          title="Home"
+        />
+        <Section 
+          id="about" 
+          subtitle={dummyText}
+          dark={false}
+          title="about"
+        />
+        <Section 
+          id="experience" 
+          subtitle={dummyText}
+          dark={true}
+          title="experience"
+        />
+        <Section 
+          id="projects" 
+          subtitle={dummyText}
+          dark={false}
+          title="projects"
+        />
+        <Section 
+          id="extras" 
+          subtitle={dummyText}
+          dark={true}
+          title="extras"
+        />
         <Footer />
       </div>
     );
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,20 +1,82 @@
 import React, { Component } from 'react';
 import './Sidebar.css';
 import profile from './profile_picture.jpg';
+import { Link, animateScroll as scroll } from "react-scroll";
 
 class Sidebar extends Component {
 
     render() {
         return (
-            <nav className="sidebar bg-dark">    
-                <img src={profile} className="img-thumbnail rounded-circle img-fluid w-75" />
-                <ul className="navbar-nav mr-auto py-4">
-                    <li className=" text-light">Charles Qian</li>
-                </ul>
-                <a className="nav-link text-light py-4" href="#introduction">Introduction</a>
-                <a className="nav-link text-light py-4" href="#about">About</a>
-                <a className="nav-link text-light py-4" href="#projects">Projects</a>
-                <a className="nav-link text-light py-4" href="#extras">Extras</a>                
+            <nav className="sidebar bg-dark" id="sidebar">
+                <div className="nav-content">
+                    <img src={profile} className="img-thumbnail rounded-circle img-fluid w-75" />
+                    <ul className="navbar-nav mr-auto py-4">
+                        <li className="text-light">Charles Qian</li>
+                    </ul>
+                    <ul className="nav-items text-light">
+                        <li className="nav-item text-light">
+                            <Link
+                                activeClass="active"
+                                to="home"
+                                spy={true}
+                                smooth={true}
+                                offset={-70}
+                                duration={500}
+                            >
+                                Home
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link
+                                activeClass="active"
+                                to="about"
+                                spy={true}
+                                smooth={true}
+                                offset={-70}
+                                duration={500}
+                            >
+                                About
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link
+                                activeClass="active"
+                                to="experience"
+                                spy={true}
+                                smooth={true}
+                                offset={-70}
+                                duration={500}
+                            >
+                                Experiences
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link
+                                activeClass="active"
+                                to="projects"
+                                spy={true}
+                                smooth={true}
+                                offset={-70}
+                                duration={500}
+                            >
+                                Projects
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link
+                                activeClass="active"
+                                to="extras"
+                                spy={true}
+                                smooth={true}
+                                offset={-70}
+                                duration={500}
+                            >
+                                Extras
+                            </Link>
+                        </li>
+                    </ul>
+                </div>    
+                             
             </nav>
         );
     }
